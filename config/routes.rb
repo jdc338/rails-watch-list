51950d0f13bf203fd34eaaeae57679f83cb0cc75
James Corfe <james.corfe@gmail.com>
2023-02-24 14:40:52 +0000

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :lists do
-    resources :bookmarks
+    resources :bookmarks, except: :destroy
   end
 
   resources :bookmarks, only: [:destroy]
